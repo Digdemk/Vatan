531fe5af93ae44098a81a00b90aa3e2d7e34dada
esmanurkilic <esmanurkilic9763@gmail.com>
2020-12-20 02:21:43 +0300

    Template Layout ve Product giydirildi.

---
+++ Vatan/Controllers/BaseeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vatan.Areas.Admin.Models.ORM.Context;
+using Vatan.Areas.Admin.Models.ORM.Entities;
+
+namespace Vatan.Controllers
+{
+    public class BaseeController : Controller
+    {
+        private readonly VatanContext _vatancontext;
+        private readonly IMemoryCache _memoryCache;
+
+        public BaseeController(VatanContext vatancontext, IMemoryCache memoryCache)
+        {
+            _vatancontext = vatancontext;
+            _memoryCache = memoryCache;
+
+
+        }
+
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            List<Category> menu = new List<Category>();
+         
+            bool isExist = _memoryCache.TryGetValue("menus", out menu);
+
+            if (!isExist)
+            {
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                    .SetAbsoluteExpiration(DateTime.Now.AddMinutes(10))
+                    .SetSlidingExpiration(TimeSpan.FromSeconds(60));
+
+                menu = _vatancontext.Categories.ToList();
+
+                _memoryCache.Set("menus", menu, cacheEntryOptions);
+            }
+
+            ViewBag.menu2 = menu;
+            base.OnActionExecuting(context);
+
+           
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            base.OnActionExecuted(context);
+        }
+    }
+}
---
+++ Vatan/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vatan.Areas.Admin.Models.ORM.Context;
+using Vatan.Areas.Admin.Models.ORM.Entities;
+using Vatan.Areas.Admin.Models.VM;
+
+namespace Vatan.Controllers
+{
+    public class ProductsController : BaseeController
+    {
+        private readonly VatanContext _vatancontext;
+
+        public ProductsController(VatanContext vatancontext, IMemoryCache memoryCache) : base(vatancontext, memoryCache)
+        {
+            _vatancontext = vatancontext;
+        }
+        public IActionResult Index()
+        {
+            List<ProductVM> movies = _vatancontext.Products.Include(x => x.ProductCategories).Include(q => q.Pictures).Where(q => q.Isdeleted == false).Select(q => new ProductVM()
+            {
+                ID = q.ID,
+                ProductName = q.ProductName,
+                Description = q.Description,
+                Quantity = q.Quantity,
+                Price = q.Price,
+
+                categories = q.ProductCategories.Where(q => q.Isdeleted == false).Select(q => q.Category).ToList(),
+                MainImagePath = q.Pictures.Select(q => q.Path).ToList()
+
+
+            }).ToList();
+
+            return View(movies);
+        }
+    }
+}
